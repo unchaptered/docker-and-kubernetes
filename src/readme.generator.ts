7270fac0de9e204b9146dede3f0cf4aa3441f04e
Minsoek, lee <workstation19961002@gmail.com>
2022-12-28 14:00:50 +0900

    Update(Docs) : new REAME for 1. Docker Core

--- src/readme.generator.ts
+++ src/readme.generator.ts
@@ -7,12 +7,8 @@ const topFolderList = getFolderList('/');
 const secTopFolderList = topFolderList.map(topFolder => getFolderList(topFolder));
 
 const folderMap = new Map();
-for (let idx = 0; idx < topFolderList.length; idx++) {
-    folderMap.set(
-        topFolderList[idx],
-        secTopFolderList[idx]
-    );
-}
+for (let idx = 0; idx < topFolderList.length; idx++)
+    folderMap.set(topFolderList[idx], secTopFolderList[idx]);
 
 const markdown = convertMarkdownFile(folderMap);
 fs.writeFileSync('./README.md', markdown, { encoding: 'utf8' });
